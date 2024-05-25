cfd619ba214ac4adbca30ce4676f17fef76b78db
nKattula222231861 <163712189+nKattula222231861@users.noreply.github.com>
2024-05-25 21:19:12 +1000

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,9 +1,11 @@
 #python Project.py
-#Imports, GPIO is for the raspberry Pi Pins and the QT imports are for the GUI
+#Imports, GPIO is for the raspberry Pi Pins and the QT imports are for the GUI. Smbus is for I2C. Finally time is so the program can wait for i2c messages
 import RPi.GPIO as GPIO
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from smbus2 import SMBus
 import sys
+import time
 
 #Sets up GUI window Class
 class Window(QMainWindow):
@@ -68,14 +70,77 @@ class Window(QMainWindow):
         
     #Reacts when the AA button is pressed
     def pressAA(self):
-        #Sends a 1 bit to the arduino
+        #Changes text to show the input
         self.typeActual.setText("AA")
-
+        self.statusActual.setText("Measuring...")
+        self.voltageActual.setText("...")
+        
+        #Sends an 1 byte over i2c to the arduno
+        with SMBus(1) as bus:
+                bus.write_byte_data(9, 0, 1)
+                
+                #Then waits to read 4 bytes of data, which is the float representing the voltage measured by the arduino. Converts the sent list to a string and then to a float
+                time.sleep(0.1)
+                listAA = bus.read_i2c_block_data(9, 0, 4)
+                stringAA = ''.join(chr(x) for x in listAA)
+                floatAA = float(stringAA.strip('\x00'))
+                                                
+                #Sets voltage to it's calculated value using string
+                self.voltageActual.setText(str(floatAA))
+                
+                #Sets status based off voltage float
+                if floatAA < 1.8 and floatAA > 1.65:
+                        self.statusActual.setText("Near Fully Charged")
+                        self.statusActual.adjustSize()
+                        
+                elif floatAA < 1.65 and floatAA > 1.5:
+                        self.statusActual.setText("Halfway Charged")
+                        self.statusActual.adjustSize()
+                        
+                elif floatAA < 1.5 and floatAA > 1.3:
+                        self.statusActual.setText("Battery Low")
+                        self.statusActual.adjustSize()
+                else:
+                        self.statusActual.setText("Battery Invalid/Dead")
+                        self.statusActual.adjustSize()
+                
     #Reacts when the AAA button is pressed
     def pressAAA(self):
-        #Sends a 2 Bit to the arduino
+        #Changes UI to represent input
         self.typeActual.setText("AAA")
+        self.statusActual.setText("Measuring...")
+        self.voltageActual.setText("...")
         
+        #Sends an 2 byte over i2c to the arduno
+        with SMBus(1) as bus:
+                bus.write_byte_data(9, 0, 2)
+                
+                #Then waits to read 4 bytes of data, which is the float representing the voltage measured by the arduino. Converts the sent list to a string and then to a float
+                time.sleep(0.1)
+                listAAA = bus.read_i2c_block_data(9, 0, 4)
+                stringAAA = ''.join(chr(x) for x in listAAA)
+                floatAAA = float(stringAAA.strip('\x00'))
+                                                
+                #Sets voltage to it's calculated value using string
+                self.voltageActual.setText(str(floatAAA))
+                
+                #Sets status based off voltage float
+                if floatAAA < 1.8 and floatAAA > 1.65:
+                        self.statusActual.setText("Near Fully Charged")
+                        self.statusActual.adjustSize()
+                        
+                elif floatAAA < 1.65 and floatAAA > 1.5:
+                        self.statusActual.setText("Halfway Charged")
+                        self.statusActual.adjustSize()
+                        
+                elif floatAAA < 1.5 and floatAAA > 1.3:
+                        self.statusActual.setText("Battery Low")
+                        self.statusActual.adjustSize()
+                else:
+                        self.statusActual.setText("Battery Invalid/Dead")
+                        self.statusActual.adjustSize()
+                        
+                        
     #Reacts when the exit button is pressed
     def pressExit(self):
         GPIO.cleanup()
@@ -88,7 +153,7 @@ def CreateWindow():
     win.show()
     sys.exit(app.exec_())
 
-#Sets up the GPIO I2C pins
+#Sets up the GPIO pins
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
